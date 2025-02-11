09162aea003cb17bb960a4dae93fd6fb8ede4714
Praise Josiah <praisechinonso21@gmail.com>
2025-02-11 14:38:01 +0100

    removed the client firebase config into firebase-client-config.json

--- firebase.ts
+++ firebase.ts
@@ -1,18 +1,8 @@
 import { getApps, initializeApp, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import config from './firbase-client-config.json' assert {type: 'json'}
 
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCFKPfK2A7s4OKDJ0xfSczVo67KlMJWF8Q",
-  authDomain: "notion-clone-c4227.firebaseapp.com",
-  projectId: "notion-clone-c4227",
-  storageBucket: "notion-clone-c4227.firebasestorage.app",
-  messagingSenderId: "43987731577",
-  appId: "1:43987731577:web:69692f29252e9d639d40a8",
-  measurementId: "G-0LK6C1319Z",
-};
-
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+const app = getApps().length === 0 ? initializeApp(config) : getApp();
 const db = getFirestore(app);
 
 export { db };
