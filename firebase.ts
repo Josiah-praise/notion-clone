a05de7c7b8fca192edf552880bbb4731df335b54
Praise Josiah <praisechinonso21@gmail.com>
2025-02-13 14:06:13 +0100

    fiexed firebase Initialization error

--- firebase.ts
+++ firebase.ts
@@ -1,7 +1,16 @@
 import { getApps, initializeApp, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-const config = JSON.parse(process.env["FIREBASE_CLIENT_CONFIG"] || '{}');
+const config = {
+  apiKey: "AIzaSyCFKPfK2A7s4OKDJ0xfSczVo67KlMJWF8Q",
+  authDomain: "notion-clone-c4227.firebaseapp.com",
+  projectId: "notion-clone-c4227",
+  storageBucket: "notion-clone-c4227.firebasestorage.app",
+  messagingSenderId: "43987731577",
+  appId: "1:43987731577:web:69692f29252e9d639d40a8",
+  measurementId: "G-0LK6C1319Z",
+};
+
 const app = getApps().length === 0 ? initializeApp(config) : getApp();
 const db = getFirestore(app);
 
